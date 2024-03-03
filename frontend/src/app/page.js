ae4516545fcce5cd6547d6575da56bc1bca85152
akerrules <addrido@gmail.com>
2024-03-02 21:40:25 -0500

    car ui update

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,26 +1,27 @@
-'use client'
+"use client";
 
 import Car from "@/component/car";
 import Sidebar from "@/component/sideBar";
 import TpBar from "@/component/tpBar";
 import Image from "next/image";
 
-
 export default function Home() {
-
   return (
-    <main className="h-screen w-full p-8">
-      <TpBar></TpBar>
-      <div className="flex">
-        {/* 1. sideBar 
+    <main className="h-screen w-full ">
+      <div className="absolute w-full z-0 overflow-hidden">
+        <Car></Car>
+      </div>
+      {/* <div className="absolute z-1 w-full h-screen bg-pink-100">h</div> */}
+      <div className="absolute  w-full z-10">
+        <TpBar></TpBar>
+        <div className="flex">
+          {/* 1. sideBar 
         2. Three3D model Car 
         3. List of things  */}
 
-        <Sidebar></Sidebar>
-        <Car></Car>
-
+          <Sidebar></Sidebar>
+        </div>
       </div>
-
     </main>
   );
 }
